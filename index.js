aaeb5e5eef476234152087b940c4b1baf15bf6b0
kaungmyatmincreaterecord@gmail.com <your@email.com>
2026-01-21 22:41:47 +0700

    test db connection

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const pool = require("./db");
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      status: "DB connected",
+      time: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "DB error",
+      error: err.message,
+    });
+  }
+});
