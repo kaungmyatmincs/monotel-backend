3152986c3dde28912a8cd5d4a70c74482096eba3
kaungmyatmincreaterecord@gmail.com <your@email.com>
2026-03-19 02:22:26 +0700

    wrong constant

--- routes/print.js
+++ routes/print.js
@@ -505,7 +505,7 @@
 
       browser = await getBrowser();
       const page = await browser.newPage();
-      await page.setContent(allPagesHTML, { waitUntil: "load", timeout: 1200000 });
+      await page.setContent(html, { waitUntil: "load", timeout: 1200000 });
       
       const pdf = await page.pdf({
         width: "720px",
